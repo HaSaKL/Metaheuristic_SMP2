bd01daa24deb73651b975900dea712ad72621e05
Hagen Salewski <salewski@wiwi.uni-kl.de>
2015-05-21 15:22:53 +0200

    Added first simple flip neighborhood, realizing the adjecent flip (flips to modules which are ONE higher or lower

---
+++ algorithm/src/SMP2_AdjElementFlip_Neighborhood.cpp
@@ -0,0 +1,69 @@
+#include "SMP2_AdjElementFlip_Neighborhood.h"
+
+
+std::string SMP2_AdjElementFlip_Neighborhood::className() const {
+	return "SMP2_AdjElementFlip_Neighborhood";
+}
+
+
+bool SMP2_AdjElementFlip_Neighborhood::cont(SMP2& _solution) {
+	// continue if another neighbor is possible; Last Neighbor checked is move down in last element
+	return !((ElementToModule.first == _solution.GetNumTask()-1) && !current_up);
+}
+
+
+bool SMP2_AdjElementFlip_Neighborhood::hasNeighbor(SMP2& _solution) {
+	// a solution has a neighbor, if there are more then two modules and at least one element
+	return (_solution.GetNumModule() > 1);
+}
+
+
+void SMP2_AdjElementFlip_Neighborhood::init(SMP2& _solution, SMP2_Flip_Neighbor& _current) {
+	// Start out with the first element
+	ElementToModule.first = 0;
+	
+	// see if the next modue needs to be wrapped around or if it is still in range
+	int maxModule = _solution.GetNumModule();
+	int newModule;
+	current_up = true;
+	if (_solution[0] == _solution.GetNumModule() -1) {
+		newModule = 0; // wrap around
+	} else {
+		newModule = _solution[0] + 1; // next (higher) module
+	}
+	
+	// set new module number for element
+	ElementToModule.second = newModule;
+	_current.setMove(0 , newModule);
+	
+	current_up = true; // since the initial move is up
+}
+
+
+void SMP2_AdjElementFlip_Neighborhood::next(SMP2& _solution, SMP2_Flip_Neighbor& _current) {
+	if(current_up) {
+	// last movement was up, now it will be down
+		if(_solution[ElementToModule.first] == 0) {
+			//wrap around at the bottom
+			ElementToModule.second = _solution.GetNumModule() - 1;
+		} else {
+			// next (lower) module
+			ElementToModule.second = _solution[ElementToModule.first] - 1;
+		}
+		// toggle up down for next move
+		current_up = false; // current is now down
+		
+	} else {
+	// last movement was down, now the next element needs to go up
+		ElementToModule.first++;
+		if (_solution[ElementToModule.first] == _solution.GetNumModule() -1) {
+			// wrap around
+			ElementToModule.second = 0; // wrap around
+		} else {
+			// next (higher) module
+			ElementToModule.second = _solution[ElementToModule.first] + 1; // next (higher) module
+		}
+		// toggle up down for next move; current is now up
+		current_up = true;
+	}
+}
---
+++ algorithm/src/SMP2_AdjElementFlip_Neighborhood.h
@@ -0,0 +1,27 @@
+#ifndef SMP2_ADJELEMENTFLIP_NEIGHBORHOOD_H
+#define SMP2_ADJELEMENTFLIP_NEIGHBORHOOD_H
+
+#include "SMP2.h"
+#include "SMP2_ElementFlip_Neighbor.h"
+
+// FIXME: This could also represent a backable or an indexed neighborhood, if this should become important
+#include <neighborhood/moNeighborhood.h>
+
+class SMP2_AdjElementFlip_Neighborhood : public moNeighborhood<SMP2_Flip_Neighbor> {
+private:
+	// The generic representation of a module flip for an element
+	std::pair<unsigned int, unsigned int> ElementToModule;
+	
+	// indicator if the current movement was a upward or downward movement
+	// toogler
+	bool current_up;
+	
+public:
+	void init(SMP2 & _solution, SMP2_Flip_Neighbor & _current);
+	bool hasNeighbor(SMP2 & _solution);
+	void next(SMP2 & _solution, SMP2_Flip_Neighbor & _current);
+	bool cont(SMP2 & _solution);
+	std::string className() const;
+};
+
+#endif // SMP2_ADJELEMENTFLIP_NEIGHBORHOOD_H
