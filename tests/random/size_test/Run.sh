54dff90702bf67691d4b6206e2469079a632bcec
Hagen Salewski <salewski@wiwi.uni-kl.de>
2015-07-02 10:40:59 +0200

    Added test results for random/size_test

--- tests/random/size_test/Run.sh
+++ tests/random/size_test/Run.sh
@@ -4,6 +4,6 @@ for file in `ls *.dat | sort -V`
 
 do
 	echo "$(tput setaf 1) Running Test on $file $(tput sgr0)"
-	./SMP2_TimeToTarget -S=19510210 -I=20 -T=240 -p=$file -o="RandomSize.csv" -l=2 -n=2 -a=R
+	./SMP2_TimeToTarget -S=19510210 -I=20 -T=1800 -p=$file -o="RandomSize.csv" -l=2 -n=2 -a=R
 
 done
